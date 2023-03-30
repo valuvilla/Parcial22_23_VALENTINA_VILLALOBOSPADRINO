085737bbd4a57668c75ff7dac3181d359bf11a89
valuvilla <valuvp@hotmail.com>
2023-03-30 08:45:05 +0200

    cambios

---
+++ EJERCIcio3.py
@@ -0,0 +1,19 @@
+# Crear clase alumno con atributos de nombre y nota
+# Crea el constructor de la clase. Añadir en el constructor un print para informar de que el alumno se ha creado con éxito
+# Crear un método llamado calificación que imprima por pantalla si el alumno ha aprobado o suspendido en base a su nota
+
+import unittest
+
+
+class Alumno:
+    def __init__(self, nombre, nota):
+        self.nombre = nombre
+        self.nota = nota
+        print("Alumno creado con éxito")
+
+    def calificacion(self,nota, nombre):
+        if self.nota >= 5:
+            print(f"El alumno {nombre} ha aprobado con {nota}")
+        else:
+            print(f"El alumno {nombre} ha suspendido {nota}")
+    
