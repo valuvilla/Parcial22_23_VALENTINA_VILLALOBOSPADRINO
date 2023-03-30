2dc44975cca92f0247cc6959aa6e8fbe7aa39c28
valuvilla <valuvp@hotmail.com>
2023-03-30 09:29:45 +0200

    porque no me dunciona

--- ej5.py
+++ ej5.py
@@ -1,6 +1,19 @@
 """
-a alianza rebelde necesita comunicarse de manera segura pero el imperio galáctico interviene todas la comunicaciones, por lo que la princesa Leia nos encarga el desarrollo de un algoritmo de encriptación para las comunicaciones rebeldes, que contemple los siguientes requerimientos:
+a alianza rebelde necesita comunicarse de manera segura 
+pero el imperio galáctico interviene todas la comunicaciones, 
+por lo que la princesa Leia nos encarga el desarrollo de un algoritmo 
+de encriptación para las comunicaciones rebeldes, que contemple los 
+siguientes requerimientos:
 
  cada carácter deberá ser encriptado a ocho caracteres;
- se deberá generar dos tablas hash para encriptar y desencriptar, para los caracteres desde el “ ” hasta el “}” -es decir desde el 32 al 125 de la tabla ASCII.
+ se deberá generar dos tablas hash para encriptar y desencriptar, 
+ para los caracteres desde el “ ” hasta el “}” -es decir desde el 
+ 32 al 125 de la tabla ASCII.
 """
+
+# Encriptar y desencriptar con tablas hash
+# Path: ej6.py
+#
+
+import random
+
