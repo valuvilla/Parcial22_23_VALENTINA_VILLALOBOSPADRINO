f7b0d102f8a8e9d41a398d11c0999bd2acf19eb1
valuvilla <valuvp@hotmail.com>
2023-03-30 09:27:38 +0200

    enunciados

---
+++ ej5.py
@@ -0,0 +1,6 @@
+"""
+a alianza rebelde necesita comunicarse de manera segura pero el imperio galáctico interviene todas la comunicaciones, por lo que la princesa Leia nos encarga el desarrollo de un algoritmo de encriptación para las comunicaciones rebeldes, que contemple los siguientes requerimientos:
+
+ cada carácter deberá ser encriptado a ocho caracteres;
+ se deberá generar dos tablas hash para encriptar y desencriptar, para los caracteres desde el “ ” hasta el “}” -es decir desde el 32 al 125 de la tabla ASCII.
+"""
