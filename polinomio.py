5c06fd552862cb5b0bca9cfde733944418599221
valuvilla <valuvp@hotmail.com>
2023-03-30 08:51:51 +0200

    crea

---
+++ polinomio.py
@@ -0,0 +1,9 @@
+# clase Polinomio
+import unittest
+from colorama import Fore, Back, Style
+from termcolor import colored
+
+#Creamos clase nodo
+class Nodo:
+    """clase nodo simplemente enlazado"""
+    
